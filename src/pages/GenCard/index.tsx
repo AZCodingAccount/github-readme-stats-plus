5e16dacb4204362a36b1d0429e5941d83a73c621
AlbertZhang <han892577@163.com>
2024-04-16 14:09:11 +0800

    feat: 新增生成卡片自定义标题功能

--- src/pages/GenCard/index.tsx
+++ src/pages/GenCard/index.tsx
@@ -30,9 +30,11 @@ const App: React.FC = () => {
   const initialValues = {
     info: [],
     username: 'AZCodingAccount',
+    custom_title: '',
     language: 'cn',
     theme: '',
     icon: true,
+    rank_icon: 'default',
     align: 'center',
   };
 
@@ -43,6 +45,7 @@ const App: React.FC = () => {
   // 热门语言卡
   const initialValues2 = {
     username: originUsername ?? 'AZCodingAccount', // 用户名
+    custom_title: '',
     language: originLanguage ?? 'cn', // 语言
     langs_count: 5, // 显示语言个数
     layout: '', // 布局
@@ -71,22 +74,31 @@ const App: React.FC = () => {
     setCheckedValues([]);
   };
 
-  // 用户点击提交显示卡片
+  // Github热门信息统计卡
   const onSubmit = (value: any) => {
     setSubmitting(true);
     // 记录信息
     originUsername = value?.username;
     originLanguage = value?.language;
     originTheme = value?.theme;
+    let res = `<img   align="${
+      value?.align
+    }" src="https://github-readme-stats.vercel.app/api?username=${value?.username}&locale=${
+      value?.language
+    }&line_height=33&show_icons=${value?.icon}&hide=${checkedValues.join(',')}&theme=${
+      value?.theme
+    }&rank_icon=${value?.rank_icon}"/>`;
+
+    // 拼接自定义标题
+    if (value?.custom_title !== '') {
+      res =
+        res.substring(0, res.length - 3) +
+        `&custom_title=${value?.custom_title}` +
+        res.substring(res.length - 3);
+    }
 
     // 处理表单数据，拼接
-    setMarkdown(
-      `<img   align="${value?.align}" src="https://github-readme-stats.vercel.app/api?username=${
-        value?.username
-      }&locale=${value?.language}&line_height=33&show_icons=${
-        value?.icon
-      }&hide=${checkedValues.join(',')}&theme=${value?.theme}"/>`,
-    );
+    setMarkdown(res);
     // 等待时间渲染
     setTimeout(() => {
       setSubmitting(false);
@@ -94,26 +106,31 @@ const App: React.FC = () => {
     setIsShow(true);
   };
 
+  // 热门语言卡
   const onLanguageSubmit = (value: any) => {
     setSubmitting(true);
     // 处理表单数据，拼接
-    setMarkdown2(
-      `<img   align="${value?.align}" src="https://github-readme-stats.vercel.app/api/top-langs/?username=${value?.username}&locale=${value?.language}&line_height=33&theme=${value?.theme}&langs_count=${value?.langs_count}"/>`,
-    );
+    let res = `<img   align="${value?.align}" src="https://github-readme-stats.vercel.app/api/top-langs/?username=${value?.username}&locale=${value?.language}&line_height=33&theme=${value?.theme}&langs_count=${value?.langs_count}"/>`;
+    // 拼接布局
     if (value?.layout !== '') {
-      setMarkdown2(
-        `<img   align="${value?.align}" src="https://github-readme-stats.vercel.app/api/top-langs/?username=${value?.username}&locale=${value?.language}&line_height=33&theme=${value?.theme}&langs_count=${value?.langs_count}&layout=${value?.layout}"/>`,
-      );
+      res =
+        res.substring(0, res.length - 3) +
+        `&layout=${value?.layout}` +
+        res.substring(res.length - 3);
     }
+    // 拼接自定义标题
+    if (value?.custom_title !== '') {
+      res =
+        res.substring(0, res.length - 3) +
+        `&custom_title=${value?.custom_title}` +
+        res.substring(res.length - 3);
+    }
+    setMarkdown2(res);
     // 等待时间渲染
     setTimeout(() => {
       setSubmitting(false);
     }, 2000);
     setIsShow(true);
-  };
-
-  const onLangNumChange = (value: number | null) => {
-    console.log(value);
   };
 
   const [cardType, setCardType] = useState('githubInfo');
@@ -149,7 +166,7 @@ const App: React.FC = () => {
             {cardType === 'githubInfo' && (
               <Form
                 labelAlign="left"
-                labelCol={{ span: 4 }}
+                labelCol={{ span: 6 }}
                 form={form}
                 initialValues={initialValues}
                 onFinish={onSubmit}
@@ -158,8 +175,12 @@ const App: React.FC = () => {
                 <Form.Item label="Github用户名" name="username">
                   <Input placeholder="请输入用户名" />
                 </Form.Item>
+                {/* 自定义标题 */}
+                <Form.Item label="卡片标题(非必填)" name="custom_title">
+                  <Input placeholder="请输入卡片标题" />
+                </Form.Item>
                 {/* 2: 隐藏部分信息 */}
-                <Form.Item label="隐藏信息" name="info" valuePropName="checked">
+                <Form.Item label="隐藏信息(非必选)" name="info" valuePropName="checked">
                   <Space>
                     <CheckboxGroup onChange={onChange} value={checkedValues}>
                       <Checkbox value="stars">Star</Checkbox>
@@ -193,7 +214,15 @@ const App: React.FC = () => {
                     <Radio value={'radical'}>radical</Radio>
                   </Radio.Group>
                 </Form.Item>
-                {/* 6：对齐方式 */}
+                {/* 6: 排名图标 */}
+                <Form.Item label="选择右边图标" name="rank_icon">
+                  <Radio.Group name="iconGroup">
+                    <Radio value={'default'}>default</Radio>
+                    <Radio value={'percentile'}>percentile</Radio>
+                    <Radio value={'github'}>github</Radio>
+                  </Radio.Group>
+                </Form.Item>
+                {/* 7：对齐方式 */}
                 <Form.Item label="对齐方式" name="align">
                   <Radio.Group buttonStyle="solid">
                     <Radio.Button value="left">左对齐</Radio.Button>
@@ -222,7 +251,7 @@ const App: React.FC = () => {
             {cardType === 'hotLanguage' && (
               <Form
                 labelAlign="left"
-                labelCol={{ span: 4 }}
+                labelCol={{ span: 6 }}
                 form={form}
                 initialValues={initialValues2}
                 onFinish={onLanguageSubmit}
@@ -230,6 +259,10 @@ const App: React.FC = () => {
                 {/* 1: 用户名 */}
                 <Form.Item label="Github用户名" name="username">
                   <Input placeholder="请输入用户名" />
+                </Form.Item>
+                {/* 自定义标题 */}
+                <Form.Item label="卡片标题(非必填)" name="custom_title">
+                  <Input placeholder="请输入卡片标题" />
                 </Form.Item>
                 {/* 2: 更换语言 */}
                 <Form.Item label="选择语言" name="language">
@@ -243,7 +276,7 @@ const App: React.FC = () => {
                 </Form.Item>
                 {/* 3: 选择个数 */}
                 <Form.Item label="显示的语言个数" name="langs_count">
-                  <InputNumber min={1} max={20} onChange={onLangNumChange} />
+                  <InputNumber min={1} max={20} />
                 </Form.Item>
                 {/* 4：选择布局 */}
                 <Form.Item label="选择布局" name="layout">
